edd178e64d4341afb1ee43a5cc1341a3047aa218
Martin <martin.n.stauber@gmail.com>
2024-01-02 02:28:06 -0600

    more screens and functionality!

---
+++ DogecoinTerminal/Pages/BackupCodePage.cs
@@ -0,0 +1,49 @@
+﻿using DogecoinTerminal.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogecoinTerminal.Pages
+{
+	[PageDef("Pages/Xml/BackupCode.xml")]
+	internal class BackupCodePage : Page
+	{
+		//We can pull in services just by adding to constructor
+		public BackupCodePage(IPageOptions options, Navigation navigation) : base(options)
+		{
+			var mnemonic = options.GetOption<string>("mnemonic");
+			
+
+			var words = mnemonic.Split(' ');
+
+			for (int i = 0; i < words.Length; i++)
+			{
+				var wordControl = GetControl<ButtonControl>($"BackupWordButton_{i}");
+
+				wordControl.Text = $"{i+1}. {words[i]}";
+			}
+
+
+			OnClick("BackButton", _ => {
+
+
+				//this creates some fliker... i'll have to address later.
+				navigation.Pop();
+
+			});
+
+
+			OnClick("NextButton", _ => {
+
+				navigation.Pop();
+
+			});
+
+
+		}
+
+
+	}
+}
---
+++ DogecoinTerminal/Pages/WalletPage.cs
@@ -0,0 +1,26 @@
+﻿using DogecoinTerminal.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogecoinTerminal.Pages
+{
+    [PageDef("Pages/Xml/Wallet.xml")]
+    internal class WalletPage : Page
+    {
+        public WalletPage(IPageOptions options) : base(options)
+        {
+            var slot = options.GetOption<IWalletSlot>("slot");
+
+            var addressTextControl = GetControl<TextControl>("AddressText");
+            addressTextControl.Text = slot.Address;
+
+
+            var balanceTextControl = GetControl<TextControl>("BalanceText");
+            balanceTextControl.Text = "Đ" + slot.CalculateBalance();
+
+		}
+    }
+}
