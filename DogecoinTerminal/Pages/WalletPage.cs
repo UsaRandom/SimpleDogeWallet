48fd8708139f11973f4e6ba68d8aa269554ea60b
Martin <martin.n.stauber@gmail.com>
2024-01-02 20:34:44 -0600

    short messages, more wallet functionality

--- DogecoinTerminal/Pages/WalletPage.cs
+++ DogecoinTerminal/Pages/WalletPage.cs
@@ -1,4 +1,5 @@
 ﻿using DogecoinTerminal.Common;
+using DogecoinTerminal.Common.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace DogecoinTerminal.Pages
     [PageDef("Pages/Xml/Wallet.xml")]
     internal class WalletPage : Page
     {
-        public WalletPage(IPageOptions options) : base(options)
+        public WalletPage(IPageOptions options, Navigation navigation, Strings strings) : base(options)
         {
             var slot = options.GetOption<IWalletSlot>("slot");
 
@@ -20,6 +21,86 @@ namespace DogecoinTerminal.Pages
 
             var balanceTextControl = GetControl<TextControl>("BalanceText");
             balanceTextControl.Text = "Đ" + slot.CalculateBalance();
+
+
+            OnClick("BackButton", async _ =>
+            {
+                navigation.Pop();
+            });
+
+
+            OnClick("RemoveButton", async _ =>
+            {
+                //ok, we want to remove this wallet.
+                //first put in a loading page for flicker
+                await navigation.PushAsync<LoadingPage>();
+
+                var numPadResult = await navigation.PromptAsync<NumPadPage>();
+
+                //We need to get the users pin confirmed on delete.
+                if (numPadResult.Response == PromptResponse.YesConfirm
+                    && slot.SlotPin == (string)numPadResult.Value)
+                {
+                    //we've confirmed pin
+                    //now lets delete the slot.
+                    slot.ClearSlot();
+					//remove loading screen.
+					navigation.Pop();
+                    //remove wallet page
+                    navigation.Pop();
+				}
+                else
+                {
+                    //just remove loading screen
+					navigation.Pop();
+				}
+
+
+			});
+
+
+
+            OnClick("UpdatePinButton", async _ =>
+            {
+				//User wants to update pin
+
+				await navigation.PushAsync<LoadingPage>();
+
+				var numPadResult = await navigation.PromptAsync<NumPadPage>(("title", strings["terminal-wallet-updatepin-newpin"]));
+
+
+				if (numPadResult.Response != PromptResponse.YesConfirm)
+				{
+					//just remove loading screen
+					navigation.Pop();
+				}
+
+                var enteredPin = (string)numPadResult.Value;
+
+				numPadResult = await navigation.PromptAsync<NumPadPage>(("title", strings["terminal-wallet-updatepin-confirmpin"]));
+
+				if (numPadResult.Response != PromptResponse.YesConfirm
+                    && enteredPin != (string)numPadResult.Value)
+				{
+
+                    //the new pin was not updated, so lets notify user!
+
+                    await navigation.PromptAsync<ShortMessagePage>(("message", "ERROR: Pin was NOT updated!"));
+
+					navigation.Pop();
+				}
+                else
+                {
+                    //update the pin
+
+                    slot.UpdateSlotPin(enteredPin);
+
+                    await navigation.PromptAsync<ShortMessagePage>(("message", "SUCCESS: Pin was updated!"));
+
+					navigation.Pop();
+
+				}
+			});
 
 		}
     }
